d3866079ff07270380684036fafd7c5fffa1c7fe
phcreery <phcreery@gmail.com>
2020-06-09 20:25:14 -0500

    music fixes again

--- server/src/controllers/MusicController.js
+++ server/src/controllers/MusicController.js
@@ -18,6 +18,9 @@ module.exports = {
         }
         // console.log(metadata); 
         for(let i in metadata){
+          if (typeof metadata[i] === 'undefined') {
+            metadata[i] = ''
+          }
           if (typeof metadata[i].title === 'undefined') {
             metadata[i].title = ''
           }
